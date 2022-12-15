59454ea207087c50d341bf84cbda8cbe267d346c
drooler <85669046+bijenmanlol@users.noreply.github.com>
2022-12-15 17:06:59 +0000

    Initial commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Crappy Google
+
+By @drooler (https://drooler.tk/)
+
+Crappy Google is a crappy version of google that looks like it drawn by a todler.
+
+Open source Google homepage by @drooler (https://drooler.tk/)<br>
+All rights go to Google inc. (https://google.com/)
---
+++ script.js
@@ -0,0 +1,36 @@
+function search(arg) {
+  elements = document.getElementsByClassName("recent")
+  if(arg){
+    document.getElementById("input-sec").style.height = "300px";
+    document.getElementById("input-sec").style.borderRadius = "15px";
+    document.getElementById("bar").style.borderBottomStyle  = "solid";
+    for(i = 0; elements.length > i; i++){
+      elements[i].style.display = "block";
+    }
+    document.getElementById("buttons2").style.display = "block";
+    document.getElementById("buttons").style.display = "none";
+  }else{
+    document.getElementById("input-sec").style.height = "50px";
+    document.getElementById("input-sec").style.borderRadius = "25px";
+    document.getElementById("bar").style.borderBottomStyle  = "none";
+    for(i = 0; elements.length > i; i++){
+      elements[i].style.display = "none";
+    }
+    document.getElementById("buttons2").style.display = "none";
+    document.getElementById("buttons").style.display = "block";
+  }
+}
+
+document.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+      up();
+    }
+});
+
+function up() {
+  if(document.getElementById("input-bar").value == ""){
+    return
+  }else{
+    window.location.href = "https://www.google.be/search?q=" + document.getElementById("input-bar").value;
+  }
+}
